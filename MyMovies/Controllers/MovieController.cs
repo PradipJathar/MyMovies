49c15a0064b00a1a0046cdf19b5cddba38f5894c
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-06-10 10:14:33 +0530

    Add project files.

---
+++ MyMovies/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using MyMovies.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMovies.Controllers
+{
+    public class MovieController : Controller
+    {
+        MyMoviesDBEntities db = new MyMoviesDBEntities();
+
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies = db.Movies.ToList();
+
+            return View(movies);
+        }
+    }
+}
